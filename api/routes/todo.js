02bc8639d809797115e21c8077c312cbde646c3d
minkyeongJ <jomk2221@naver.com>
2023-11-17 17:45:30 +0900

    Feat: 이미지 업로드 기능 구현 중

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -1,12 +1,14 @@
-import { body, validationResult } from 'express-validator';
+import { body, validationResult } from "express-validator";
 
-import model from '../models/todo.js';
-import express from 'express';
+import model from "../models/todo.js";
+import express from "express";
+
+import multer from "multer";
 
 const router = express.Router();
 
 // 할일 목록 조회
-router.get('/todolist', function(req, res, next) {
+router.get("/todolist", function (req, res, next) {
   // #swagger.tags = ['필수 기능']
   // #swagger.summary  = '할일 목록 조회'
   // #swagger.description = '할일 목록을 조회합니다.<br>page, limit 파라미터는 선택사항이며 전달하지 않으면 전체 할일 목록을 조회합니다.<br>page만 전달할 경우 limit 값은 기본 10으로 지정됩니다.'
@@ -32,23 +34,46 @@ router.get('/todolist', function(req, res, next) {
       schema: { $ref: '#/definitions/Error500' }
     }
   */
-  try{
+  try {
     const result = model.list(req.query);
-    res.json({ok: 1, ...result});
-  }catch(err){
+    res.json({ ok: 1, ...result });
+  } catch (err) {
     next(err);
   }
 });
 
+// 이미지 저장 경로와 파일명 설정
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "db/images/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+// 파일 필터링 설정 (예: 이미지 파일만 허용)
+const fileFilter = function (req, file, cb) {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("이미지 파일만 업로드할 수 있습니다."));
+  }
+};
+
+// Multer 미들웨어 생성
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
 // 할일 등록
-router.post('/todolist', [
-  body('title').trim().notEmpty(),
-  body('content').trim().notEmpty(),
-], function(req, res, next) {
-  // #swagger.tags = ['필수 기능']
-  // #swagger.summary  = '할일 등록'
-  // #swagger.description = '할일을 등록합니다.<br>title, content를 전달하면 할일을 등록한 후 등록된 할일을 반환합니다.'
-  /* 
+router.post(
+  "/todolist",
+  upload.single("image"),
+  [body("title").trim().notEmpty(), body("content").trim().notEmpty()],
+  function (req, res, next) {
+    // #swagger.tags = ['필수 기능']
+    // #swagger.summary  = '할일 등록'
+    // #swagger.description = '할일을 등록합니다.<br>title, content를 전달하면 할일을 등록한 후 등록된 할일을 반환합니다.'
+    /* 
     #swagger.parameters['item'] = {
       required: 'true',
       in: 'body',
@@ -71,23 +96,26 @@ router.post('/todolist', [
     }
   */
 
-  try{
-    const result = validationResult(req);
-    if(result.isEmpty()){
-      const item = model.create(req.body);
-      res.json({ok: 1, item});
-    }else{
-      const error = new Error(`"${result.errors[0].path}" 항목은 필수입니다.`);
-      error.status = 422;
-      next(error);
-    }    
-  }catch(err){
-    next(err);
+    try {
+      const result = validationResult(req);
+      if (result.isEmpty()) {
+        const item = model.create(req.body, req);
+        res.json({ ok: 1, item });
+      } else {
+        const error = new Error(
+          `"${result.errors[0].path}" 항목은 필수입니다.`
+        );
+        error.status = 422;
+        next(error);
+      }
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // 할일 상세 조회
-router.get('/todolist/:_id', function(req, res, next) {
+router.get("/todolist/:_id", function (req, res, next) {
   // #swagger.tags = ['필수 기능']
   // #swagger.summary  = '할일 상세 조회'
   // #swagger.description = '할일 상세 정보를 조회합니다.'
@@ -105,20 +133,20 @@ router.get('/todolist/:_id', function(req, res, next) {
       schema: { $ref: '#/definitions/Error500' }
     }
   */
-  try{
+  try {
     const item = model.findById(Number(req.params._id));
-    if(item){
-      res.json({ok: 1, item});
-    }else{
+    if (item) {
+      res.json({ ok: 1, item });
+    } else {
       next();
     }
-  }catch(err){
+  } catch (err) {
     next(err);
   }
 });
 
 // 할일 수정
-router.patch('/todolist/:_id', function(req, res, next) {
+router.patch("/todolist/:_id", function (req, res, next) {
   // #swagger.tags = ['필수 기능']
   // #swagger.summary  = '할일 수정'
   // #swagger.description = '할일을 수정합니다. 할일을 수정한 후 수정된 할일을 반환합니다.<br>바디로 전달한 속성에 대해서만 수정되고 전달하지 않은 속성은 유지됩니다.'
@@ -144,20 +172,20 @@ router.patch('/todolist/:_id', function(req, res, next) {
       schema: { $ref: '#/definitions/Error500' }
     }
   */
-  try{
+  try {
     const item = model.update(Number(req.params._id), req.body);
-    if(item){
-      res.json({ok: 1, item});
-    }else{
+    if (item) {
+      res.json({ ok: 1, item });
+    } else {
       next();
     }
-  }catch(err){
+  } catch (err) {
     next(err);
   }
 });
 
 // DB 초기화
-router.delete('/todolist/init', async function(req, res, next) {
+router.delete("/todolist/init", async function (req, res, next) {
   // #swagger.tags = ['부가 기능']
   // #swagger.summary  = '데이터 베이스 초기화'
   // #swagger.description = '데이터 베이스를 초기화 합니다.<br>기존 데이터는 모두 삭제된 후 할일 목록 4개가 추가되고 추가된 할일 목록을 반환합니다.'
@@ -171,16 +199,16 @@ router.delete('/todolist/init', async function(req, res, next) {
       schema: { $ref: '#/definitions/Error500' }
     }
   */
-  try{
+  try {
     const result = await model.init();
-    res.json({ok: 1, ...result});
-  }catch(err){
+    res.json({ ok: 1, ...result });
+  } catch (err) {
     next(err);
   }
 });
 
 // 할일 삭제
-router.delete('/todolist/:_id', function(req, res, next) {
+router.delete("/todolist/:_id", function (req, res, next) {
   // #swagger.tags = ['필수 기능']
   // #swagger.summary  = '할일 삭제'
   // #swagger.description = '할일을 삭제합니다.'
@@ -198,14 +226,14 @@ router.delete('/todolist/:_id', function(req, res, next) {
       schema: { $ref: '#/definitions/Error500' }
     }
   */
-  try{
+  try {
     const result = model.remove(Number(req.params._id));
-    if(result > 0){
-      res.json({ok: 1});
-    }else{
+    if (result > 0) {
+      res.json({ ok: 1 });
+    } else {
       next();
     }
-  }catch(err){
+  } catch (err) {
     next(err);
   }
 });
